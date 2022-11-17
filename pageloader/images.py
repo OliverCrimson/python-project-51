373b66e7532cc9757bef611e775ddb0a0a91eae5
MonkeyBusiness <crimson.oliver@gmail.com>
2022-11-17 13:27:25 +0300

    2nd step commit

--- pageloader/images.py
+++ pageloader/images.py
@@ -5,67 +5,51 @@ import pathlib
 import urllib.parse
 import urllib.request
 from urllib.parse import urljoin, urlparse
-    
 
 from pageloader.loader import namin
 
-x = "/home/monkeybusiness/new_proj/python-project-51/pageloader/page-loader-hexlet-repl-co/page-loader-hexlet-repl-co.html"
-
-# with open(x, 'r') as file:
-#     scr = file.read()
-# 
-# soup = BeautifulSoup(scr, 'html.parser')
-# 
-# # title = soup.title
-# # print(title.string)
-# ima = soup.find_all('img')
-# for i in ima:
-#     print(i['src'])
-
 
 def find_img_tags(link):
-    with open(link, 'r') as file:
-        scr = file.read()
-        file.close()
+    """
+    finding all img tags in soup object, saving content of tags into list object.
+    correcting content of img tag in soup and writing corrected html document.
+    correctd html doc means src content of img tag lead to local file
+    :param link: 
+    :return: list of img ['src'] content
+    """
+    cur_dir = pathlib.Path.cwd()
+    valid_naming = namin(link)
+    desirable_folder = pathlib.Path(f"{cur_dir}/{valid_naming[:-5]}_files")
+    desirable_folder.mkdir(exist_ok=True)
+    desirable_link = f"{valid_naming[:-5]}"
+    scr = requests.get(link).content
     soup = BeautifulSoup(scr, 'html.parser')
     images_links = soup.find_all('img')
     images_row = []
     for item in images_links:
         images_row.append(item['src'])
+        item['src'] = f"{valid_naming[:-5]}_files/{desirable_link}.png"
+    with open(f"{desirable_folder}/{valid_naming}", "w") as file:
+        file.write(soup.prettify())
     return images_row
 
 
-# bbb = find_img_tags(x)
-# # print(find_img_tags(x))
-# # def splitting_link(link):
-# #     some_link = urllib.parse.urlparse(link)
-# #     print(some_link)
-# fff = urlparse("https://page-loader.hexlet.repl.co")
-# print(fff)
-# # rrrr = urljoin('https://page-loader.hexlet.repl.co', bbb[0])
-# # 
-# req = requests.get(rrrr)
-# with open('sample.jpg', 'wb') as file:
-#     file.write(req.content)
-
-def download_imges(link):
-    valid_name = namin(link)
-    print(valid_name)
-    valid_link = f'{valid_name[:-5]}/{valid_name}'
-    parsed_link = urlparse(link)
-    print(parsed_link)
-    print(valid_link)
-    img_paths_array = find_img_tags(link)
-    print(img_paths_array)
-    with open(f"{pathlib.Path.cwd()}{valid_link}", "r") as file:
-        scrape = file.read()
-        soup = BeautifulSoup(scrape, 'html.parser')
-        imgs = soup.find_all('img')
-        for item in imgs:
-            item['src'] = valid_link
-            
+find_img_tags('https://page-loader.hexlet.repl.co')
+qwe = ['/assets/professions/nodejs.png']
 
 
-download_imges(x)
+def img_links_array(array, link):
+    form_array = []
+    for i in array:
+        form_array.append(urljoin(link, i))
+    return form_array
 
 
+def downloading_imgs(link, array):
+    for item in array:
+        with open(f"{namin(link)[:-5]}_files/{namin(link)[:-5]}.png", 'wb') as file:
+            file.write(requests.get(item).content)
+
+
+aaa = img_links_array(qwe, "https://page-loader.hexlet.repl.co")
+downloading_imgs("https://page-loader.hexlet.repl.co", aaa)
