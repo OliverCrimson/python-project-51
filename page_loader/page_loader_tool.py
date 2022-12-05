e59f60cb0f12daca94802feaeaacdea51088229b
MonkeyBusiness <crimson.oliver@gmail.com>
2022-12-05 13:05:12 +0300

    fixing wfs

--- page_loader/page_loader_tool.py
+++ page_loader/page_loader_tool.py
@@ -47,7 +47,7 @@ def download(link, folder=''):# noqa
     with open(f"{x}/{name}.html", "w") as file:
         file.write(response.prettify())
         logging.info(f'write html file: '
-                     f'{pathlib.Path.cwd()}/{folder}{name}.html')
+                     f'{x}/{name}.html')
     big_list = lst + lst_links + script_links
     result = img_links_array(big_list, link)
     downloading_imgs(link, result, val_path)
