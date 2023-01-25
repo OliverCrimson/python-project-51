daf8d7afe4d36a5818a4b9b0f0d6d8359aea2ce2
MonkeyBusiness <crimson.oliver@gmail.com>
2023-01-25 15:09:28 +0300

    added try/exept statement

--- page_loader/mkfolders.py
+++ page_loader/mkfolders.py
@@ -4,12 +4,11 @@ from page_loader.naming import naming_folders
 
 
 def make_folder(link, folder=''):
-    try:
-        pathlib.Path(folder).exists()
-        path = pathlib.Path(folder)
-        directory = pathlib.Path(f"{path}/{naming_folders(link)}")
-        directory.mkdir(parents=True, exist_ok=True)
-        logging.info(f'Created directory {directory}')
-        return directory
-    except FileNotFoundError as ex:
-        raise ex
+        if pathlib.Path(folder).exists():
+            path = pathlib.Path(folder)
+            directory = pathlib.Path(f"{path}/{naming_folders(link)}")
+            directory.mkdir(parents=True, exist_ok=True)
+            logging.info(f'Created directory {directory}')
+            return directory
+        else:
+            raise FileNotFoundError
