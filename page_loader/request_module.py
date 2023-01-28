ebbcc47d0baacca1ae8cdd9f5c4c15be130b7043
MonkeyBusiness <crimson.oliver@gmail.com>
2023-01-28 17:22:52 +0300

    fixing

--- page_loader/request_module.py
+++ page_loader/request_module.py
@@ -1,4 +1,4 @@
-# import logging
+import logging
 # import sys
 
 import requests
@@ -6,14 +6,13 @@ from bs4 import BeautifulSoup
 
 
 def requesting(link):
-    try:
-        respond = requests.get(link)
-        respond.raise_for_status()
-        soup = BeautifulSoup(respond.content, 'html.parser')
-        return soup
-    except Exception as error:
-        return error
+    
+    respond = requests.get(link)
+    respond.raise_for_status()
+    logging.info(f'respond {respond.raise_for_status()}')
     # if respond.status_code != 200:
     #     logging.warning(f"respond status code: "
     #                     f"{respond.status_code}")
-    #     raise Exception
+    #     raise Exception 
+    soup = BeautifulSoup(respond.content, 'html.parser')
+    return soup
