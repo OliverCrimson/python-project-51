546cd3e9e8015c4f0641498621987d4a873423b1
MonkeyBusiness <crimson.oliver@gmail.com>
2023-01-29 22:35:20 +0300

    updating tests

--- tests/test_page_loader_tool.py
+++ tests/test_page_loader_tool.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 import tempfile
 import requests_mock
@@ -12,16 +11,25 @@ from page_loader.naming import change_name
 
 FIXTURES_PATH = f'{pathlib.Path.cwd()}/tests/fixtures'
 LINK = 'https://site.com/blog/about'
-
-
-def test_change_name():
-    assert change_name('https://page-loader.hexlet.repl.co') == 'page-loader-hexlet-repl-co'
+FAKE_IMAGE = 'https://site.com/photos/me.jpg'
+FAKE_SCRIPT = 'https://site.com/assets/scripts.js'
+FAKE_CSS = 'https://site.com/blog/about/assets/styles.css'
+ORIGINAL = f'{FIXTURES_PATH}/original.html'
+EXPECTED = f'{FIXTURES_PATH}/expected.html'
+IMAGE = f'{FIXTURES_PATH}/site-com-blog-about_files/site-com-photos-me.jpg'
+SCRIPT = f'{FIXTURES_PATH}/site-com-blog-about_files/site-com-assets-scripts.js'
+STYLES = f'{FIXTURES_PATH}/site-com-blog-about_files/' \
+         f'site-com-blog-about-assets-styles.css'
 
 
 def read_from_file(file, mode):
     with open(file, mode) as f:
         f = f.read()
         return f
+
+
+def test_change_name():
+    assert change_name(LINK) == 'site-com-blog-about'
 
 
 def test_string():
@@ -32,23 +40,17 @@ def test_string():
 
 def test_folder_creation():
     expected_dir_name = 'site-com-blog-about_files'
-    make_folder('https://site.com/blog/about')
-    assert pathlib.Path(expected_dir_name).exists()
-    os.rmdir(expected_dir_name)
-
-
-def test_download():
     with tempfile.TemporaryDirectory() as tempdir:
-        expect = f'{tempdir}/page-loader-hexlet-repl-co.html'
-        assert expect == download('https://page-loader.hexlet.repl.co', tempdir)
+        make_folder('https://site.com/blog/about', tempdir)
+        assert pathlib.Path(f'{tempdir}/{expected_dir_name}').exists()
 
 
 def test_content():
-    original = read_from_file(f'{FIXTURES_PATH}/original.html', 'r')
-    expected = read_from_file(f'{FIXTURES_PATH}/expected.html', 'r')
-    image = read_from_file(f'{FIXTURES_PATH}/site-com-blog-about_files/site-com-photos-me.jpg', 'rb')
-    script = read_from_file(f'{FIXTURES_PATH}/site-com-blog-about_files/site-com-assets-scripts.js', 'rb')
-    style = read_from_file(f'{FIXTURES_PATH}/site-com-blog-about_files/site-com-blog-about-assets-styles.css', 'r')
+    original = read_from_file(ORIGINAL, 'r')
+    expected = read_from_file(EXPECTED, 'r')
+    image = read_from_file(IMAGE, 'rb')
+    script = read_from_file(SCRIPT, 'rb')
+    style = read_from_file(STYLES, 'r')
     with requests_mock.Mocker() as mock:
         fake_adress = 'https://site.com/blog/about'
         fake_image = 'https://site.com/photos/me.jpg'
@@ -58,9 +60,9 @@ def test_content():
         mock.get(fake_image, content=image)
         mock.get(fake_script, content=script)
         mock.get(fake_style, text=style)
-        # x = requesting(fake_adress)
-        # y = finding_tags(x, fake_adress)
         with tempfile.TemporaryDirectory() as tempdir:
             final = download(fake_adress, tempdir)
             result = read_from_file(final, 'r')
+            expected_download_result = f'{tempdir}/site-com-blog-about.html'
             assert result == expected
+            assert final == expected_download_result
